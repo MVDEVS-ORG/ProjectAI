e7e5e607d0450d586e0446e7fb57b132be95db99
Mohit Chaudhary <66890438+MohitChaudhary2@users.noreply.github.com>
2025-06-11 12:48:14 +0530

    [FTM-41] Added Addressables and Created AssetService Class

---
+++ Assets/Services/AssetService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Assets.Services
+{
+    public class AssetService : IAssetService
+    {
+        public async Task<GameObject> InstantiateAsync(string address)
+        {
+            GameObject go = await Addressables.InstantiateAsync(address).Task;
+            return go;
+        }
+
+        public async Task<T> LoadAssetAsync<T>(string address)
+        {
+            var asset = await Addressables.LoadAssetAsync<T>(address).Task;
+            return asset;
+        }
+
+        public void UnloadAsset<T>(T asset)
+        {
+            Addressables.Release(asset);
+        }
+    }
+}
---
+++ Assets/Services/IAssetService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Services
+{
+    public interface IAssetService
+    {
+        Task<T> LoadAssetAsync<T>(string address);
+        Task<GameObject> InstantiateAsync(string address);
+        void UnloadAsset<T>(T asset);
+    }
+}
