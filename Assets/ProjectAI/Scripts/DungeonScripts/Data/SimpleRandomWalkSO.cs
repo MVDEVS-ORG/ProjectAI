5b37552385745ef3eb09347d547ccc1b66cc39f5
Mohit Chaudhary <66890438+MohitChaudhary2@users.noreply.github.com>
2025-06-14 19:33:08 +0530

    [FTM-43] Created Procedural Dungeon

---
+++ Assets/ProjectAI/Scripts/DungeonScripts/Data/SimpleRandomWalkSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+namespace Assets.DungenGame.Scripts.NewAlgo.Data
+{
+    [CreateAssetMenu(fileName = "SimpleRandomWalkParameters_", menuName = "PCG/SimpleRandomWalkData")]
+    public class SimpleRandomWalkSO : ScriptableObject
+    {
+        public int iterations = 10, walkLength = 10;
+        public bool startRandomlyEachIteration = true;
+    }
+}
