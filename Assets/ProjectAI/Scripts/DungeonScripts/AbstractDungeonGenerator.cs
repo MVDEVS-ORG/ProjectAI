e0ad7e6c296be64d64d20ecef2192a65cc4d105c
Mohit Chaudhary <66890438+MohitChaudhary2@users.noreply.github.com>
2025-06-16 10:10:42 +0530

    [FTM-43] Created Procedural Dungeon (#5)
    * [FTM-43] Created Procedural Dungeon
    
    * [FTM-43] Updated Namespaces
    
    * Error fix

---
+++ Assets/ProjectAI/Scripts/DungeonScripts/AbstractDungeonGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.ProjectAI.Scripts.DungeonScripts
+{
+    public abstract class AbstractDungeonGenerator: MonoBehaviour
+    {
+        [SerializeField]
+        protected TilemapVisualizer tilemapVisualizer = null;
+        [SerializeField]
+        protected Vector2Int startPosition = Vector2Int.zero;
+
+        public void GenerateDungeon()
+        {
+            tilemapVisualizer.Clear();
+            RunProceduralGeneration();
+        }
+
+        protected abstract void RunProceduralGeneration();
+    }
+}
