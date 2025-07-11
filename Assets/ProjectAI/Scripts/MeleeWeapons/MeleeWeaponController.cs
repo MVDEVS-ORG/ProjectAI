a811cb174790627875b14f3d270eb5e56a59c016
Malhar Choure <chouremalhar@gmail.com>
2025-07-11 12:28:02 +0530

    changes to machete

--- Assets/ProjectAI/Scripts/MeleeWeapons/MeleeWeaponController.cs
+++ Assets/ProjectAI/Scripts/MeleeWeapons/MeleeWeaponController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MeleeWeaponController : IMeleeWeaponController
@@ -12,10 +11,15 @@ public class MeleeWeaponController : IMeleeWeaponController
 
     private bool _initialized = false;
 
+    private Coroutine _attackCoroutine;
+    private bool _isLeftSwing = false;
+    private CharacterView _playerView;
+
     public bool Initialized => _initialized;
 
     void IMeleeWeaponController.Initialize(Transform playerTransform, Transform cursorTransform)
     {
+        _playerView = playerTransform.GetComponent<CharacterView>();
         _playerTransform = playerTransform;
         _cursorTransform = cursorTransform;
     }
@@ -28,16 +32,15 @@ public class MeleeWeaponController : IMeleeWeaponController
 
     void IMeleeWeaponController.MeleeAttack()
     {
-
-        if(_view.IsAttacking())
+        if(IsAttacking())
         {
             return;
         }
         else if(_model.Attacks<_model.AttackChainLimit)
         {
-            _view.Attack();
+            Attack();
             _model.Attacks = _model.Attacks+1;
-            _view.StartCoroutine(AttackCoolDown());
+            _playerView.StartCoroutine(AttackCoolDown());
         }
     }
 
@@ -45,5 +48,43 @@ public class MeleeWeaponController : IMeleeWeaponController
     {
         yield return Awaitable.WaitForSecondsAsync(_model.AttackRechargeDelay);
         _model.Attacks--;
+    }
+
+    private void Attack()
+    {
+        if (_playerTransform != null && _cursorTransform != null && _attackCoroutine == null)
+        {
+            _isLeftSwing = !_isLeftSwing;
+            float startingAngle = 0;
+            float endAngle = 0;
+            Vector2 direction = (_cursorTransform.position - _playerTransform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x);
+            startingAngle = _isLeftSwing ? angle - Mathf.PI / 2 : angle + Mathf.PI / 2;
+            endAngle = _isLeftSwing ? angle + Mathf.PI / 2 : angle - Mathf.PI / 2;
+            _view.gameObject.SetActive(true);
+            _attackCoroutine = _view.StartCoroutine(MeleeMotion(_isLeftSwing, startingAngle, endAngle));
+        }
+    }
+
+    IEnumerator MeleeMotion(bool isLeftSwing, float startAngle, float endAngle)
+    {
+        int directionOfMotion = 0;
+        directionOfMotion = isLeftSwing ? 1 : -1;
+        float angle = startAngle;
+        while (directionOfMotion * angle < directionOfMotion * endAngle)
+        {
+            angle = angle + (directionOfMotion * Time.deltaTime) / (_model.AttackTime);
+            _view.transform.position = _playerTransform.position + new Vector3(_model.DistanceFromPlayer * Mathf.Cos(angle), _model.DistanceFromPlayer * Mathf.Sin(angle), 0);
+            _view.transform.right = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+            yield return Awaitable.EndOfFrameAsync();
+        }
+        //_view.transform.position = -100 * Vector3.one;
+        _attackCoroutine = null;
+        _view.gameObject.SetActive(false);
+    }
+
+    private bool IsAttacking()
+    {
+        return _attackCoroutine != null;
     }
 }
--- Assets/ProjectAI/Scripts/MeleeWeapons/MeleeWeaponView.cs
+++ Assets/ProjectAI/Scripts/MeleeWeapons/MeleeWeaponView.cs
@@ -17,45 +17,9 @@ public class MeleeWeaponView : MonoBehaviour
     {
         _playerTransform = playerTransform;
         _cursorTransform = cursorTransform;
+        transform.parent = _playerTransform;
         _model = new MeleeWeaponModel(_meleeData);
         return _model;
-    }
-
-    public void Attack()
-    {
-        if (_playerTransform != null && _cursorTransform != null && _attackCoroutine == null)
-        {
-            _isLeftSwing = !_isLeftSwing;
-            float startingAngle = 0;
-            float endAngle = 0;
-            Vector2 direction = (_cursorTransform.position - _playerTransform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x);
-            startingAngle = _isLeftSwing ? angle - Mathf.PI / 2 : angle + Mathf.PI / 2;
-            endAngle = _isLeftSwing ? angle + Mathf.PI / 2 : angle - Mathf.PI / 2;
-            _attackCoroutine = StartCoroutine(MeleeMotion(_isLeftSwing, startingAngle, endAngle));
-        }
-    }
-
-    public bool IsAttacking()
-    {
-        return _attackCoroutine != null;
-    }
-
-    IEnumerator MeleeMotion(bool isLeftSwing,float startAngle,float endAngle)
-    {
-        int directionOfMotion = 0;
-        directionOfMotion = isLeftSwing ? 1 : -1;
-        float angle = startAngle;
-        while(directionOfMotion*angle < directionOfMotion*endAngle)
-        {
-            angle = angle + (directionOfMotion * Time.deltaTime) / (_model.AttackTime);
-            transform.position = _playerTransform.position + new Vector3(_model.DistanceFromPlayer * Mathf.Cos(angle), _model.DistanceFromPlayer * Mathf.Sin(angle),0);
-            transform.right = new Vector3(Mathf.Cos(angle),  Mathf.Sin(angle),0); 
-            yield return Awaitable.EndOfFrameAsync();
-        }
-        transform.position = -100 * Vector3.one;
-        _meleeAttacksNo++;
-        _attackCoroutine = null;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
