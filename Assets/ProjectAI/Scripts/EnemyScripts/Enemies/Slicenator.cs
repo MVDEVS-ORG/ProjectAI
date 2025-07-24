d0eba1b6a70513638d785771ab128594eee5f371
Mohit Chaudhary <66890438+MohitChaudhary2@users.noreply.github.com>
2025-07-24 10:56:18 +0530

    [FTM-58] Fixed Enemy and added new Enemy Slicenator

---
+++ Assets/ProjectAI/Scripts/EnemyScripts/Enemies/Slicenator.cs
@@ -0,0 +1,30 @@
+﻿using Assets.ProjectAI.Scripts.EnemyScripts.AttackBehaviour;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.ProjectAI.Scripts.EnemyScripts.Enemies
+{
+    public class Slicenator : EnemyAI
+    {
+        public override void Initialize(HealthModels model)
+        {
+            base.Initialize(model);
+        }
+        public override void InitializeStates()
+        {
+            stateMap.Add(new IdleState(), EnemyStateTypes.Idle);
+            stateMap.Add(new SearchState(), EnemyStateTypes.Search);
+            stateMap.Add(new ChaseState(), EnemyStateTypes.Chase);
+            stateMap.Add(new DeadState(), EnemyStateTypes.Dead);
+            stateMap.Add(new PatrolState(), EnemyStateTypes.Patrol);
+            stateMap.Add(new MeleeAttack(), EnemyStateTypes.Attack);
+
+        }
+
+        public void Attack()
+        {
+            Debug.LogError(currentState);
+            (currentState as MeleeAttack)?.Attack();
+        }
+    }
+}
